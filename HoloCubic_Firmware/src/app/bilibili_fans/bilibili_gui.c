3233151f2b22d0591b53b91c7f474f41003cd673
climbsnail <climbsnail@qq.com>
2021-07-19 09:37:10 +0800

    add bilibili_gui.c

---
+++ HoloCubic_Firmware/src/app/bilibili_fans/bilibili_gui.c
@@ -0,0 +1,24 @@
+#include "bilibili_gui.h"
+
+#include "lv_port_indev.h"
+#include "lvgl.h"
+
+void bilibili_gui_init(void)
+{
+
+}
+
+
+/*
+ * 其他函数请根据需要添加
+ */
+
+void display_bilibili(const char *file_name, lv_scr_load_anim_t anim_type)
+{
+
+}
+
+void bilibili_gui_del(void) 
+{
+
+}
--- HoloCubic_Firmware/src/app/bilibili_fans/bilibili_gui.h
+++ HoloCubic_Firmware/src/app/bilibili_fans/bilibili_gui.h
@@ -1,5 +1,24 @@
-#ifndef APP_BILIBILI_ICO_H
-#define APP_BILIBILI_ICO_H
+#ifndef APP_BILIBILI_GUI_H
+#define APP_BILIBILI_GUI_H
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+#include "lvgl.h"
+#define ANIEND                      \
+    while (lv_anim_count_running()) \
+        lv_task_handler(); //等待动画完成
+
+    void bilibili_gui_init(void);
+    void display_bilibili(const char *file_name, lv_scr_load_anim_t anim_type);
+    void bilibili_gui_del(void);
+
+#ifdef __cplusplus
+} /* extern "C" */
+#endif
+
 
 #ifdef __cplusplus
 extern "C"
