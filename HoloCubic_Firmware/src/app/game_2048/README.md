188cb080b9badfe2ec13343a5acfe0b426e55ed7
climbsnail <climbsnail@qq.com>
2021-11-15 02:04:11 +0800

    md

--- HoloCubic_Firmware/src/app/game_2048/README.md
+++
@@ -1,2 +0,0 @@
-### 2048游戏
-APP由`AndyXFuture`编写，原项目链接为`https://github.com/AndyXFuture/HoloCubic-2048-anim`
--- HoloCubic_Firmware/src/app/game_2048/game_2048.h
+++
@@ -1,8 +0,0 @@
-#ifndef APP_GAME_2048_H
-#define APP_GAME_2048_H
-
-#include "sys/interface.h"
-
-extern APP_OBJ game_2048_app;
-
-#endif
